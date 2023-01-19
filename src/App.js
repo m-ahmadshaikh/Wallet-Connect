c8f968e95c10da04dbbae738057e6c720c899a93
ahmadbilal-authornate <bilal@authornate.com>
2023-01-20 02:16:31 +0500

    Updated the code

--- src/App.js
+++ src/App.js
@@ -2,70 +2,114 @@ import { useState } from "react";
 import "./App.css";
 import Button from "./components/Button/Button";
 import Web3 from "web3";
-// import WalletConnectProvider from "@walletconnect/web3-provider";
-
-//  Create WalletConnect Provider
-// const provider = new WalletConnectProvider({
-//   infuraId: "27e484dcd9e3efcfd25a83a78777cdf1", // Required
-// });
-
-
-//  Create Web3
-// let web3 = new Web3(provider);
-let web3;
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import { Web3Auth } from "@web3auth/modal";
 
 function App() {
   const [selectedNetwork, setSelectedNetwork] = useState("");
   const [selectedWallet, setSelectedWallet] = useState("");
   const [address, setAddress] = useState("");
   const [balance, setBalance] = useState("");
+  const [error, setError] = useState(null);
+
+ 
 
   const onConnect = async () => {
-    console.log(selectedWallet)
-    console.log(selectedNetwork)
-    if (selectedNetwork === "ethereum") {
-      console.log('eth')
+   
+    
+    if (selectedNetwork === 'ethereum') {
+      window.web3 = new Web3(window.ethereum);
 
-      web3 = new Web3(window.web3.currentProvider);
-
-    } else if (selectedNetwork === "bnb") {
-      console.log('bnb')
-
-      web3 = new Web3(
-        new Web3.providers.HttpProvider("https://bsc-dataseed.binance.org/")
-      );
-    } else if (selectedNetwork === "polygon") {
-      console.log('polygon')
-      web3 = new Web3(
-        new Web3.providers.HttpProvider("https://polygon.network")
-      );
     }
-    if (selectedWallet === "metamask" && window.ethereum) {
-      console.log('metamask')
+
+    else if (selectedNetwork === 'polygon') {
+      // window.web3 = new Web3(new Web3.providers.HttpProvider("https://rpc-mumbai.polygon.io"));
+      const web3auth = new Web3Auth({
+        clientId: "BHgVODRbv330NID6h2jcmLkCFXIuVO5ntveQW7IRPcleCVs060EJQzvFabWCrmZQ835ozQdL9fRaG-k4Dlt1Ej0", // get it from Web3Auth Dashboard
+        web3AuthNetwork: "cyan",
+        chainConfig: {
+          chainNamespace: "eip155",
+          chainId: "0x89", // hex of 137, polygon mainnet
+          rpcTarget: "https://rpc.ankr.com/polygon",
+          // Avoid using public rpcTarget in production.
+          // Use services like Infura, Quicknode etc
+          displayName: "Polygon Mainnet",
+          blockExplorer: "https://polygonscan.com",
+          ticker: "MATIC",
+          tickerName: "Matic",
+        },
+      });
+  await web3auth.initModal();
+  
+  const web3authProvider = web3auth.connect();
+  
+   window.web3 = new Web3(web3authProvider); 
+
+    }
+    else if (selectedNetwork === 'bnb') {
+      // window.web3 = new Web3('https://bsc-dataseed1.binance.org:443');
+      const web3auth = new Web3Auth({
+        clientId: "BHgVODRbv330NID6h2jcmLkCFXIuVO5ntveQW7IRPcleCVs060EJQzvFabWCrmZQ835ozQdL9fRaG-k4Dlt1Ej0", // get it from Web3Auth Dashboard
+        web3AuthNetwork: "cyan",
+        chainConfig: {
+          chainNamespace: "eip155",
+          chainId: "0x38", // hex of 56
+          rpcTarget: "https://rpc.ankr.com/bsc",
+          // Avoid using public rpcTarget in production.
+          // Use services like Infura, Quicknode etc
+          displayName: "Binance SmartChain Mainnet",
+          blockExplorer: "https://bscscan.com/",
+          ticker: "BNB",
+          tickerName: "BNB",
+        },
+      });
+      await web3auth.initModal();
+      
+      const web3authProvider = web3auth.connect();
+      
+       window.web3 = new Web3(web3authProvider); //
+
+    }
+
+
+
+    if(selectedWallet === 'metamask'){
+      if(!window.ethereum){
+        setError('Install MetaMask Extention')
+        return;
+      }
       try {
-        await window.ethereum.enable();
-        const accounts = await web3.eth.getAccounts();
-        setAddress(accounts[0]);
-        const balanceWei = await web3.eth.getBalance(accounts[0]);
-        const balanceEther = web3.utils.fromWei(balanceWei, "ether");
-        setBalance(balanceEther);
-      } catch (error) {}
-    } else {
-      console.log('install metamask')
-      // if (provider.connected) {
-      //   // const provider = new WalletConnectProvider({
-      //   //   infuraId: "27e484dcd9e3efcfd25a83a78777cdf1",
-      //   // });
-      //   // await provider.enable();
-      //   // if (provider.connected) {
-      //   //   const accounts = await web3.eth.getAccounts();
-      //   //   setAddress(accounts[0]);
-      //   //   const balanceWei = await web3.eth.getBalance(accounts[0]);
-      //   //   const balanceEther = web3.utils.fromWei(balanceWei, "ether");
-      //   //   setBalance(balanceEther);
-      //   // }
-      // }
+        const address = (await window.web3.eth.getAccounts())[0];
+
+        // Get user's balance in ether
+        const balance = window.web3.utils.fromWei(
+          await window.web3.eth.getBalance(address) // Balance is in wei
+        );
+        setAddress(address);
+       
+        setBalance(balance);
+    } catch (error) {
+        console.log(error);
     }
+  }else {
+    const walletConnectProvider = new WalletConnectProvider({
+      infuraId: "27e484dcd9e3efcfd25a83a78777cdf1", // Optional
+  });
+  window.web3 = new Web3(walletConnectProvider);
+  try {
+      await walletConnectProvider.enable();
+      const accounts = await window.web3.eth.getAccounts();
+      setAddress(accounts[0]);
+      const balanceWei = await window.web3.eth.getBalance(accounts[0]);
+      const balanceEther = window.web3.utils.fromWei(balanceWei, 'ether');
+      setBalance(balanceEther);
+  } catch (error) {
+      console.log(error);
+  }
+  }
+
+   
+
   };
   const onNetworkClick = (text) => {
     setSelectedNetwork(text);
@@ -112,6 +156,8 @@ function App() {
         <button onClick={onConnect} className="btn">
           Connect Wallet
         </button>
+
+        {error && <div className="error">{error}</div>}
       </div>
 
       <div>
